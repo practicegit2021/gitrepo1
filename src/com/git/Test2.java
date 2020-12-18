3919f0b4016bdefecc9383b4127869bf050b4303
Software Developer <Software Developer@DESKTOP-RVAE6E2.mshome.net>
2020-12-18 23:44:47 +0530

    Changed the Code and Filename too. Review the code , approve and merge it.

---
+++ src/com/git/Test2.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("I am Team Lead");
+
+		int a=10,b=2,c=0;
+		c=a*b;
+		System.out.println("Result is"+c);
+	}
+
+}
